e190cf098808074cd5110b926a2329943797fbb3
QuackNinja <60707853+QuackNinja@users.noreply.github.com>
2021-01-01 04:19:06 +0300

    Added 'support' for colors (not only B/W) / issues with brightness

--- cvmain.py
+++ cvmain.py
@@ -13,13 +13,20 @@ def convolute():
     #yes, I know it is verry brute forced and totally not efficient on this state, or any state
     for i in range(1, height-1):
         for j in range(1, width-1):
-            sum = 0
+            red = green = blue = 0
             for ii in range(3):
                 for jj in range(3):
-                    sum += img[i+ii-1][j+jj-1][0] * kernel[ii][jj] # basically assuming the image is B/W
-            sum /= 9
-
-            conv_img[i][j] = [sum, sum, sum]
+                    
+                    current = img[i+ii-1][j+jj-1]
+                    red += current[0] * kernel[ii][jj]
+                    green += current[1] * kernel[ii][jj]
+                    blue += current[2] * kernel[ii][jj]
+            
+            red /= 9
+            green /= 9
+            blue /= 9
+            
+            conv_img[i][j] = [red, green, blue]
     #also I will take care of the borders and corners another day, not the most important for now I think
 
 
@@ -34,6 +41,6 @@ kernel = np.loadtxt("kernel.txt")
 conv_img = np.zeros((height, width, channels))
 
 convolute()
-show_images()
+#show_images()
 
-cv2.imwrite("da22.png", conv_img)
+cv2.imwrite("convoluted_image.png", conv_img)
