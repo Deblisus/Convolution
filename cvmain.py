ce8983eb3fc80f07d31833d3570816a2be0ee600
QuackNinja <60707853+QuackNinja@users.noreply.github.com>
2021-01-01 03:21:19 +0300

    'Migrated' project to opencv because it actualy works, unlike pillow

---
+++ cvmain.py
@@ -0,0 +1,7 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("da.png")
+
+print(type(img))
+print(img[0][1])
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from PIL import Image
 
-filename = "da.png"
+def print_img(image):
+    for i in range(image.width):
+        for j in range(image.height):
+            print(image.getpixel((j, i)), end=" ")
+        print()
+
+filename = "download.jpg"
 
 img = Image.open(filename)
-
-for i in range(img.width):
-    for j in range(img.height):
-        print(img.getpixel((j, i)), end=" ")
-    print()
-
+#print_img(img)
 print()
-print(img.getpixel((0, 1)))
-print(img.getpixel((1, 0)))
+print(img.mode)
+
+img.convert(mode="L")
+img.save("pic.jpg")
