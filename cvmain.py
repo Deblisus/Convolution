a962889700b23204964d772392e495f9119a4b9f
QuackNinja <60707853+QuackNinja@users.noreply.github.com>
2021-01-03 13:43:21 +0300

    added extremely awful 'documentation'

--- cvmain.py
+++ cvmain.py
@@ -1,13 +1,40 @@
 import cv2
 import numpy as np
 
+#Should probably make all of this into a class but idk
+'''
+Brief explanation:
+    Trying to make a convolution algorithm from scratch
+    not getting way to complicated or something
+
+    functions:
+        to_gray_scale: Basically the title, takes the image and converts it to grayscale
+                        for example for the edge detection kernels
+        convolute: Again, basically the title, takes the selected image and applies the
+                        kernel on it (kinda brute forcing)
+    
+    kernel: 
+        Using a 3x3 kernel, the first 3 lines
+        The 4th line is some parameters:
+            1st number: the number to be divided by to keep the luminance 'accurate' (probably will 'automate'
+                            it by summing the values in the kernel and avoiding zero)
+            2nd number: 1 or 0, to be converted to gray scale or not to be converted to gray scale
+            3rd number: TBD, maybe for the edge detection to apply both sides
+                            (left oriented and right oriented edge detection kernel) to the image
+            
+'''
+
 def to_gray_scale():
     for i in range(height):
         for j in range(width):
             R, G, B = img[i][j]
             #gs_value = (int(R) + int(G) + int(b)) / 3 
             gs_value = (int(min(R, G, B)) + int(max(R, G, B))) / 2 #gray-scale value
-            img[i][j] = [gs_value, gs_value, gs_value]
+            img[i][j] = [gs_value, gs_value, gs_value] 
+            # I know that I can save about 3 times the memory by ghanging from rgb,
+            # but it will not work well with the convolution function, but
+            # I probaably will fix that in the future i guess and until then 
+            # it will work just fine
 
 def show_images():
     cv2.imshow('image', img)
@@ -47,10 +74,12 @@ kernel = np.loadtxt("kernel.txt")
 #initializing new convoluted picture
 conv_img = np.zeros((height, width, channels))
 
+#converting to gray scale
 if kernel[3][1] == 1:
     to_gray_scale()
 
 convolute()
 #show_images()
 
+#saving the image (why did I type this?)
 cv2.imwrite("convoluted_image.png", conv_img)
