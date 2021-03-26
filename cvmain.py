87b27a9b596b2e5033cb95166f07b7e4a035ae68
Deblisus <60707853+Deblisus@users.noreply.github.com>
2021-03-26 09:07:32 +0300

    fixed black borders by cutting them, threading still has some issues

--- cvmain.py
+++ cvmain.py
@@ -1,7 +1,9 @@
 import cv2
 import numpy as np
 import time
-import concurrent.futures
+#import concurrent.futures
+#import multiprocessing
+import threading
 
 start_time = time.time()
 
@@ -34,33 +36,23 @@ Brief explanation:
 '''
 
 
-def threading(threads):
-    if threads == 0:
-        threads = 1
-    
-    dist = int(height / threads)
-    times = list(range(0, height, dist))
-    times.pop()
-    print(times)
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        #process = executor.map(threading_convolute, times)
-        process = [executor.submit(threading_convolute, pos, dist) for pos in times]
 
-def threading_convolute(start_pos, distance):
+def threading_convolute(start_pos, end_pos, index):
     #yes, I know it is verry brute forced and totally not efficient on this state, or any state
     spacer = int(kernel[-1][2]/2)
     size = int(kernel[-1][2])
+    #print(start_pos)
 
     actual_kernel = kernel[0:size]
-    print(1)
+    #print(end_pos+spacer, height-spacer)
+    print(index)
 
     divider = calc_divider(actual_kernel)
-    print(start_pos+distance)
-    for i in range(start_pos+spacer, start_pos+distance+spacer):
+    for i in range(start_pos+spacer, end_pos+spacer):
         for j in range(spacer, width-spacer):
             # This ain't pretty, but definately much faster. Finally implemented matrix multiplication
-            
+
             curr_bgr_section = img[i-spacer:i+spacer+1, j-spacer:j+spacer+1]
             
             blue_section = curr_bgr_section[0:size, 0:size, 0]
@@ -73,6 +65,30 @@ def threading_convolute(start_pos, distance):
             
             conv_img[i][j] = [blue, green, red]
     #also I will take care of the borders and corners another day, not the most important functionality for now I think
+
+    
+def threadng(threads):
+    if threads == 0:
+        threads = 1
+    
+    dist = int(height / threads)
+    times = list(range(0, height, dist))
+    times.pop()
+    print(times)
+    print(dist)
+
+    #with concurrent.futures.ThreadPoolExecutor() as executor:
+        #process = executor.map(threading_convolute, times)
+        #process = [executor.submit(threading_convolute, pos, dist) for pos in times]
+    processes = []
+    for strt in times:
+        t = threading.Thread(target=threading_convolute, args=(strt, strt+dist, strt//dist))
+        t.start()
+        processes.append(t)
+    
+    for t in processes:
+        t.join()
+
 
 
 def to_gray_scale():
@@ -133,7 +149,7 @@ def convolute():
 
 
 #reading image obv
-img = cv2.imread("picture.jpg")
+img = cv2.imread("pic.jpg")
 height, width, channels = img.shape
 
 kernel = np.loadtxt("kernel.txt")
@@ -145,9 +161,11 @@ conv_img = np.zeros((height, width, channels))
 if kernel[-1][1] == 1:
     to_gray_scale()
 
-threading(kernel[-1][0])
-#convolute()
+#threadng(kernel[-1][0])
+convolute()
 #show_images()
+
+conv_img = conv_img[1:height-1, 1:width-1]
 
 
 #saving the image (why did I type this?)
